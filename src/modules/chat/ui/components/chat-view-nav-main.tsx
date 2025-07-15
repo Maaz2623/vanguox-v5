da57c35702739e63b99522945e55ab8bfba2e952
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2025-07-16 00:02:34 +0530

    modified:   src/modules/chat/ui/components/chat-view-nav-main.tsx 	modified:   src/modules/chat/ui/components/chat-view-nav-secondary.tsx

--- src/modules/chat/ui/components/chat-view-nav-main.tsx
+++ src/modules/chat/ui/components/chat-view-nav-main.tsx
@@ -22,6 +22,7 @@ export function ChatViewNavMain({
 }) {
   const router = useRouter();
 
+
   return (
     <SidebarGroup>
       <SidebarGroupContent className="flex flex-col gap-2">
--- src/modules/chat/ui/components/chat-view-nav-secondary.tsx
+++ src/modules/chat/ui/components/chat-view-nav-secondary.tsx
@@ -22,21 +22,23 @@ export function ChatViewNavSecondary({
   }[];
 } & React.ComponentPropsWithoutRef<typeof SidebarGroup>) {
   return (
-    <SidebarGroup {...props}>
-      <SidebarGroupContent>
-        <SidebarMenu>
-          {items.map((item) => (
-            <SidebarMenuItem key={item.title}>
-              <SidebarMenuButton asChild>
-                <a href={item.url}>
-                  <item.icon />
-                  <span>{item.title}</span>
-                </a>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-          ))}
-        </SidebarMenu>
-      </SidebarGroupContent>
-    </SidebarGroup>
+    <>
+      <SidebarGroup {...props}>
+        <SidebarGroupContent>
+          <SidebarMenu>
+            {items.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild>
+                  <a href={item.url}>
+                    <item.icon />
+                    <span>{item.title}</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </SidebarGroup>
+    </>
   );
 }
